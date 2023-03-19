a6416675ac952d08889b9b98e42f97f9c4b468d6
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-03-19 00:32:06 -0700

    s

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program generates a random number and
+ *              determines if it is positive or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet);
+		}
+		alphabet += 1;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
