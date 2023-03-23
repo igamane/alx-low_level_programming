1a8b3a85e047b331921e36a16137a03217740c8e
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-03-22 20:15:56 -0700

    s

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * void print_alphabet_x10 - print lowercase alphabet
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int counter = 1;
+
+	while (counter <= 10)
+	{
+		char alphabet;
+
+                alphabet = 'a';
+
+                while (alphabet <= 'z')
+                {
+                    _putchar(alphabet);
+                    alphabet++;
+                }
+                _putchar('\n');
+
+		counter++;
+	}
+}
