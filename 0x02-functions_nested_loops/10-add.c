aabe8b3b24b140cff846a744fd91e27db2665ec9
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-03-28 23:11:36 -0700

    s

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - add two numbers
+ *
+ * Description: add two numbers
+ *
+ * @a: first number
+ *
+ * @b: second number
+ *
+ * Return: result
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
