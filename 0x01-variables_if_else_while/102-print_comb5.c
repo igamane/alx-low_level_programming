4e5988b50f64d088792b80d4c5346608c467c651
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-03-28 19:41:16 -0700

    s

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program generates a random number and
+ *              determines if it is positive or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	void check_and_print(int first, int second, int third, int fourth)
+	{
+		if (first != third || second != fourth)
+		{
+			if (first != 0 ||
+				second != 0 ||
+				third != 0 ||
+				fourth != 1)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			putchar(first + '0');
+			putchar(second + '0');
+			putchar(' ');
+			putchar(third + '0');
+			putchar(fourth + '0');
+		}
+	}
+	int first_digit;
+
+	first_digit = 0;
+
+	while (first_digit <= 9)
+	{
+		int second_digit;
+
+		second_digit = 0;
+
+		while (second_digit <= 9)
+		{
+			int third_digit;
+
+			third_digit = 0;
+
+			while (third_digit <= 9)
+			{
+				int fourth_digit;
+
+				fourth_digit = 0;
+
+				while (fourth_digit <= 9)
+				{
+					check_and_print(first_digit,
+							second_digit,
+							third_digit,
+							fourth_digit);
+					fourth_digit++;
+				}
+				third_digit++;
+			}
+			second_digit++;
+		}
+		first_digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
