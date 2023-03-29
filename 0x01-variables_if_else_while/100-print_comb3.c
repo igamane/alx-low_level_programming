0ab01415cdc94b4fb8f2ede45ae5f8d734e6d82b
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-03-28 18:10:31 -0700

    s

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program generates a random number and
+ *              determines if it is positive or negative.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int first_digit;
+
+	first_digit = 0;
+
+	while (first_digit <= 9)
+	{
+		int second_digit;
+
+		second_digit = 0;
+
+		while (second_digit <= 9)
+		{
+			if (first_digit != second_digit && first_digit < second_digit)
+			{
+				putchar(first_digit + '0');
+				putchar(second_digit + '0');
+				putchar(',');
+				putchar(' ');
+			}
+			second_digit++;
+		}
+		first_digit++;
+	}
+
+	putchar('\n');
+
+	return(0);
+}
