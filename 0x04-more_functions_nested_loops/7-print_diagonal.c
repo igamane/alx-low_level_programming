8732d7535577ba251b2fd56689e217026efb2f69
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-03-29 22:56:13 -0700

    s

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonals
+ *
+ * Description: print diagonals
+ *
+ * @n: the parameter
+ *
+ * Return: None
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		int j;
+
+		for (j = 0; j < i; j ++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+}
