0a7fd77a920fc3e23a99c62b6c6b1627e4f4d849
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-04-01 00:28:53 -0700

    s

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_rev - print in reverse
+ *
+ * Description: print in reverse
+ *
+ * @s: the string
+ *
+ * Return: None
+ */
+
+void print_rev(char *s)
+{
+	char *temp;
+
+	temp = s;
+
+	while (*s != '\0')
+	{
+		s++;
+	}
+
+	while (s >= temp)
+	{
+		_putchar(*s);
+		s--;
+	}
+
+	_putchar('\n');
+}
