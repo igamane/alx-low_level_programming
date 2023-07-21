30fe0aa8f75d20d2b287530f4f1dcfc3d0811606
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-07-20 17:42:40 -0700

    s

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - power
+ *
+ * Description: the power
+ *
+ * @x: the number
+ *
+ * @y: the power
+ *
+ * Return: the power
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	} else if (y < 0)
+	{
+		return (-1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
