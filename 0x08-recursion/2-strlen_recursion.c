45ce0e5a71d604e7e19459bdc05cc338d7258dd1
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-07-20 17:20:17 -0700

    s

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strlen_recursion(char *s) - the lenght of a string
+ * _strlen_recursion - the lenght of a string
  *
  * Description: theh
  *
@@ -14,9 +14,7 @@ int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		return 1 + _strlen_recursion(s + 1);
-	} else
-	{
-		return 0;
+		return (1 + _strlen_recursion(s + 1));
 	}
+	return 0;
 }
