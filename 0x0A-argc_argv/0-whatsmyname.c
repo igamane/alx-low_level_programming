ff6605453e96f063fbff947d25b349680aac0d1c
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-07-22 07:05:39 -0700

    s

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main entry
+ *
+ * Description: the main function
+ *
+ * @argc: arguments count
+ *
+ * @argv: arguments vector
+ *
+ * Return: always 0
+ */
+
+int main(int argc __attribute__ ((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
