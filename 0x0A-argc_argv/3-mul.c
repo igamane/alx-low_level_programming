84b753e7b65d1d443205a41dc3a5ec0e27600bc4
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-07-22 07:38:38 -0700

    s

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/** main - main
+ *
+ * Description: dhg
+ *
+ * @argc: hdg
+ * 
+ * @argv: hf
+ *
+ * Return: 0 jfh
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	} else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+
+	return (0);
+}
