ab774a687573b4d469f9ca242f0bc05f08a1d213
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-07-25 18:03:31 -0700

    s

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,10 +23,12 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
+		s1 = malloc(sizeof(char));
 		s1[0] = '\0';
 	}
 	if (s2 == NULL)
 	{
+		s2 = malloc(sizeof(char));
 		s2[0] = '\0';
 	}
 
