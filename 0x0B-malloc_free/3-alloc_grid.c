d568a77ec5e79f619623eab7be90976e91df90e3
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-07-25 18:43:08 -0700

    s

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,55 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - jhf
+ *
+ * Description: hfj
+ *
+ * @width: jf
+ *
+ * @height: jhfj
+ *
+ * Return: jfd
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	array = malloc(height * sizeof(int *));
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+
+		if (array[i] == NULL)
+		{
+			int k;
+
+			for (k = 0; k < i; k++)
+			{
+				free(array[k]);
+			}
+			free(array);
+			return (NULL);
+		}
+
+		for (j = 0; j < width; j++)
+		{
+			array[i][j] = 0;
+		}
+	}
+
+	return (array);
+}
