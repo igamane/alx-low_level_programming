6c1665009757e787977d22f5f7a872ee834bc066
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-07-25 18:12:02 -0700

    s

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,46 +23,34 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s1 = malloc(sizeof(char));
-		s1[0] = '\0';
+		s1 = malloc(sizeof(char)), s1[0] = '\0';
 	}
 	if (s2 == NULL)
 	{
-		s2 = malloc(sizeof(char));
-		s2[0] = '\0';
+		s2 = malloc(sizeof(char)), s2[0] = '\0';
 	}
-
 	temp = s1;
 	size1 = 0;
 	size2 = 0;
-
 	while (*temp != '\0')
 	{
-		size1++;
-		temp++;
+		size1++, temp++;
 	}
 	temp = s2;
 	while (*temp != '\0')
 	{
-		size2++;
-		temp++;
+		size2++, temp++;
 	}
 	str = malloc((size1 + size2 + 1) * sizeof(char));
-
 	if (str == NULL)
-	{
 		return (NULL);
-	}
-
 	for (count1 = 0; count1 < size1; count1++)
 	{
 		str[count1] = s1[count1];
 	}
-
 	for (count2 = count1; count2 <= (count1 + size2); count2++)
 	{
 		str[count2] = s2[count2 - count1];
 	}
-
 	return (str);
 }
