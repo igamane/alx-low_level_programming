94da55b64e7055be6aae33bd235b39a5dba353c1
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-07-26 16:20:55 -0700

    s

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,93 +1,46 @@
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * str_concat - jdfj
+ * *string_nconcat - concatenates n bytes of a string to another string
+ * @s1: string to append to
+ * @s2: string to concatenate from
+ * @n: number of bytes from s2 to concatenate to s1
  *
- * Description: jhsfj
- *
- * @concat: sfjhjf
- * @s1: sjhf
- *
- * @s2: dhjf
- *
- * @size1: sjh
- *
- * @size2: sjhsfj
- *
- * Return: sf
+ * Return: pointer to the resulting string
  */
-char *str_concat(char *concat,
-		char *s1,
-		char *s2,
-		unsigned int size1,
-		unsigned int size2)
-{
-	unsigned int i, j;
-
-	for (i = 0; i < size1; i++)
-	{
-		concat[i] = s1[i];
-	}
-	for (j = size1; j < (size1 + size2); j++)
-	{
-		concat[j] = s2[j - size1];
-	}
-	concat[j] = '\0';
-
-	return (concat);
-}
-
-/**
- * string_nconcat - jdfj
- *
- * Description: jhsfj
- *
- * @s1: sjhf
- *
- * @s2: dhjf
- *
- * @n: jfjs
- *
- * Return: sf
- */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat, *temp;
-	unsigned int size1, size2;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = malloc(sizeof(char)), s1[0] = '\0';
-	if (s2 == NULL)
-		s2 = malloc(sizeof(char)), s2[0] = '\0';
-	temp = s1, size1 = 0, size2 = 0;
-	while (*temp != '\0')
-	{
-		size1++, temp++;
-	}
-	temp = s2;
-	while (*temp != '\0')
-	{
-		size2++, temp++;
-	}
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
 
-	if (n >= size2)
-	{
-		concat = malloc(size1 * (size2 + 1) * sizeof(char));
-	} else
-	{
-		concat = malloc(size1 * (n + 1) * sizeof(char));
-	}
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (concat == NULL)
-	{
+	if (!s)
 		return (NULL);
-	}
 
-	if (n >= size2)
+	while (i < len1)
 	{
-		return (str_concat(concat, s1, s2, size1, size2));
+		s[i] = s1[i];
+		i++;
 	}
 
-	return (str_concat(concat, s1, s2, size1, n));
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }
+
