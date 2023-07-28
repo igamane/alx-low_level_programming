dcaef6a3a4ea0bfe72c7f6ab05f10da31c57fbb9
Mohamed Igamane <mohamedigamane1203@gmail.com>
2023-07-27 18:47:47 -0700

    s

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dog.h"
+
+/**
+ * free_dog - fjhjd
+ *
+ * @d: skfh
+ *
+ * Description: sff
+ *
+ * Return: none
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+
+	free(d);
+}
